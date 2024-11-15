7c1f245dd47c677abdd4d65deb1b1a7eb5a43e10
Denis Kovalev <d.kovalev@arenadata.io>
2024-11-15 14:03:04 +0300

    initial implementation

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,72 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type taskQueue struct {
+	mtx   sync.Mutex
+	tasks []Task
+}
+
+func (q *taskQueue) getNewTask() Task {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+
+	if len(q.tasks) == 0 {
+		return nil
+	}
+
+	task := q.tasks[0]
+	q.tasks = q.tasks[1:]
+
+	return task
+}
+
+func NewTaskQueue(tasks []Task) *taskQueue {
+	return &taskQueue{
+		tasks: tasks,
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+
+	var nerrors atomic.Int32
+
+	queue := NewTaskQueue(tasks)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() error {
+			defer wg.Done()
+
+			for task := queue.getNewTask(); task != nil; task = queue.getNewTask() {
+
+				if err := task(); err != nil {
+					nerrors.Add(1)
+				}
+
+				if int(nerrors.Load()) >= m {
+					return ErrErrorsLimitExceeded
+				}
+			}
+			return nil
+		}()
+	}
+
+	wg.Wait()
+
+	if int(nerrors.Load()) >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
