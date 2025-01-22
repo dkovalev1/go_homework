44cb9573fe0dc6b9c171ad22b9ec20f85c4b83b9
Denis Kovalev <d.kovalev@arenadata.io>
2025-01-22 13:09:37 +0500

    Hw05 parallel execution (#5)
    * initial implementation
    
    * Add custom sleep
    
    * restore go.sum
    
    * Make the linter happy
    
    * do the task with channel, revert changes to test and go.sum

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,40 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var nerrors atomic.Int32
+	var wg sync.WaitGroup
+
+	queue := make(chan Task, len(tasks))
+
+	for _, t := range tasks {
+		queue <- t
+	}
+
+	close(queue)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() error {
+			defer wg.Done()
+
+			for task := range queue {
+				if err := task(); err != nil {
+					nerrors.Add(1)
+				}
+
+				if m > 0 && int(nerrors.Load()) >= m {
+					return ErrErrorsLimitExceeded
+				}
+			}
+			return nil
+		}()
+	}
+
+	wg.Wait()
+
+	if m > 0 && int(nerrors.Load()) >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -8,13 +8,38 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:all
 	"go.uber.org/goleak"
 )
+
+/*
+require.Eventually uses a timer to measure duration interval with a periodic call
+of condition function to check the status as we can see in assertions.go:1636.
+Similar mechanism with a timer is used in Sleep() as we can see in:
+https://github.com/golang/go/blob/5164a865e3de723f07976edac234c4d6a814128e/src/runtime/time.go#L283.
+
+Our task requires us to use require.Eventually*() instead of time.Sleep() but since it's virtually the same,
+let's make our own simple Sleep() implementation with a timer.
+*/
+func sleepFor(interval time.Duration) {
+	timer := time.NewTimer(interval)
+	defer timer.Stop()
+
+	<-timer.C
+}
 
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
+	t.Run("check sleep implementation", func(t *testing.T) {
+		start := time.Now()
+		duration := 400 * time.Millisecond
+		sleepFor(duration)
+		end := time.Now()
+
+		expected := start.Add(duration)
+		require.True(t, end.After(expected) || end.Equal(expected))
+	})
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -58,6 +83,35 @@ func TestRun(t *testing.T) {
 
 		workersCount := 5
 		maxErrorsCount := 1
+
+		start := time.Now()
+		err := Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+	t.Run("No errors checked if m <= 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				sleepFor(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 0
 
 		start := time.Now()
 		err := Run(tasks, workersCount, maxErrorsCount)
