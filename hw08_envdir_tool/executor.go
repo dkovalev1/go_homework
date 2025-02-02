8009223fccee80200de81619bfefbbe241bbd4d1
Denis Kovalev <d.kovalev@arenadata.io>
2025-02-02 20:03:33 +0500

    hw08 is complete

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,59 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func removeEnvVar(s []string, deleteKey string) []string {
+	if s == nil {
+		return nil
+	}
+
+	for k, v := range s {
+		parts := strings.SplitN(v, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		key := parts[0]
+		if key == deleteKey {
+			return append(s[:k], s[k+1:]...)
+		}
+	}
+	return s
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	execCmd := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+	execCmd.Stdin = os.Stdin
+
+	execCmd.Env = os.Environ()
+	for key, value := range env {
+		if key == "" {
+			fmt.Println("Error: environment variable key is empty")
+			return 1
+		}
+		if value.NeedRemove {
+			execCmd.Env = removeEnvVar(execCmd.Env, key)
+		} else {
+			execCmd.Env = append(execCmd.Env, fmt.Sprintf("%s=%s", key, value.Value))
+		}
+	}
+
+	if err := execCmd.Run(); err != nil {
+		fmt.Printf("Error executing command: %v\n", err)
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require" //nolint
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	ret := RunCmd([]string{"ls"}, nil)
+	require.Equal(t, 0, ret)
+
+	ret = RunCmd([]string{"cat", "/wrong/path"}, nil)
+	require.NotEqual(t, 0, ret)
+
+	ret = RunCmd([]string{"uname", "-a"}, nil)
+	require.Equal(t, 0, ret)
+
+	ret = RunCmd([]string{"bash", "-c", "echo \"testvar=$MYTEST\""}, Environment{"MYTEST": {"test", false}})
+	require.Equal(t, 0, ret)
 }
