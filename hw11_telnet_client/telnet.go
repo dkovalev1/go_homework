c3d1425ad821e80bb5e36cea7f6730ac91a3c7e6
Denis Kovalev <d.kovalev@arenadata.io>
2025-02-16 14:17:19 +0500

    Initial implementation

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +16,58 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+func (c *TelnetClientImpl) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *TelnetClientImpl) Close() error {
+	return c.conn.Close()
+}
+
+func (c *TelnetClientImpl) Send() error {
+	buf := make([]byte, 1024)
+	n, err := c.in.Read(buf)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			fmt.Fprintf(os.Stderr, "EOF\n")
+		}
+		return err
+	}
+	_, err = c.conn.Write(buf[:n])
+	return err
+}
+
+func (c *TelnetClientImpl) Receive() error {
+	buf := make([]byte, 1024)
+	n, err := c.conn.Read(buf)
+	if err != nil {
+		if errors.Is(err, net.ErrClosed) || errors.Is(err, io.EOF) {
+			fmt.Fprintf(os.Stderr, "connection closed\n")
+		}
+		return err
+	}
+	_, err = c.out.Write(buf[:n])
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &TelnetClientImpl{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
