d80c57a4629323526fd6303fe9fc49b544122784
Denis Kovalev <d.kovalev@arenadata.io>
2025-02-16 22:56:01 +0500

    Initial implementation (#11)
    hw11 is ready

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,96 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10, "Connection timeout in seconds")
+}
+
+func usage() {
+}
+
+func doTelnet(host string) error {
+	client := NewTelnetClient(host, timeout, os.Stdin, os.Stdout)
+
+	// Handle SIGINT (Ctrl-C) and SIGTERM properly
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "connected to: %s\n", host)
+
+	exit := make(chan struct{})
+
+	// Send data
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+
+				err := client.Send()
+				if err != nil {
+					exit <- struct{}{}
+					return
+				}
+			}
+		}
+	}()
+
+	// Receive data
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				err := client.Receive()
+				if err != nil {
+					exit <- struct{}{}
+					return
+				}
+			}
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Fprintln(os.Stderr, "Received SIGINT, closing connection...")
+	case <-exit:
+	}
+	client.Close()
+	return nil
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	host := net.JoinHostPort(flag.Args()[0], flag.Args()[1])
+
+	err := doTelnet(host)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -8,10 +8,40 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint
 )
 
+type BufferCloser struct {
+	*bytes.Buffer
+	closed bool
+}
+
+func (b *BufferCloser) Close() error {
+	b.closed = true
+	return nil
+}
+
+func (b *BufferCloser) Read(p []byte) (n int, err error) {
+	if b.closed {
+		return 0, io.EOF
+	}
+	return b.Buffer.Read(p)
+}
+
+func NewBufferCloser(data string) *BufferCloser {
+	return &BufferCloser{Buffer: bytes.NewBufferString(data)}
+}
+
 func TestTelnetClient(t *testing.T) {
+	t.Run("Connect", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		c := NewTelnetClient(l.Addr().String(), 0, nil, nil)
+		require.NoError(t, c.Connect())
+	})
+
 	t.Run("basic", func(t *testing.T) {
 		l, err := net.Listen("tcp", "127.0.0.1:")
 		require.NoError(t, err)
@@ -58,6 +88,57 @@ func TestTelnetClient(t *testing.T) {
 			n, err = conn.Write([]byte("world\n"))
 			require.NoError(t, err)
 			require.NotEqual(t, 0, n)
+		}()
+
+		wg.Wait()
+	})
+
+	t.Run("EOF", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			in := NewBufferCloser("hello world\n")
+			out := &bytes.Buffer{}
+
+			timeout, err := time.ParseDuration("10s")
+			require.NoError(t, err)
+
+			client := NewTelnetClient(l.Addr().String(), timeout, in, out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			require.NoError(t, client.Send())
+			require.NoError(t, client.Receive())
+
+			require.NoError(t, in.Close())
+
+			err = client.Send()
+			require.ErrorIs(t, err, io.EOF)
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			defer func() { require.NoError(t, conn.Close()) }()
+
+			request := make([]byte, 1024)
+			nr, err := conn.Read(request)
+			require.NoError(t, err)
+			require.NotEqual(t, 0, nr)
+
+			nw, err := conn.Write(request)
+			require.NoError(t, err)
+			require.NotEqual(t, 0, nw)
 		}()
 
 		wg.Wait()
