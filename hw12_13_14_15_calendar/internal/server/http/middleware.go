6fb4a23e7ca42e6708aa13dbc05f8a9c6485b271
Denis Kovalev <d.kovalev@arenadata.io>
2025-03-03 00:37:28 +0500

    Update hw14 (#14)
    Update hw14

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -1,11 +1,35 @@
 package internalhttp
 
 import (
+	"fmt"
 	"net/http"
+	"time"
+
+	logger "github.com/dkovalev1/go_homework/hw12_13_14_15_calendar/internal/logger" //nolint
 )
 
-func loggingMiddleware(next http.Handler) http.Handler { //nolint:unused
+func logRequest(r *http.Request, duration time.Duration, logger *logger.Logger) {
+	logger.Info(
+		fmt.Sprintf("%s [%s] %s %s %v %v \"%s\"\n",
+			r.RemoteAddr,
+			time.Now().Format(time.RFC3339),
+			r.Method,
+			r.URL.Path,
+			r.Proto,
+			duration,
+			r.UserAgent(),
+		),
+	)
+}
+
+func loggingMiddleware(logger *logger.Logger, next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO
+		start := time.Now()
+		next(w, r)
+
+		duration := time.Since(start)
+
+		logRequest(r, duration, logger)
 	})
 }
